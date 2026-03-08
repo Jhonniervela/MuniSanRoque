2f455cf1cfa792ff091eeddc7bd21eb5fdbeb5ae
jhonniervela <brayan.vela@upeu.edu.pe>
2026-03-08 15:03:51 -0400

    Correccion de iconos para Android

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Instalado');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
